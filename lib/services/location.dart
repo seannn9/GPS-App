c632aef4eb0ddb6b1a81a16337d035e4c7ac6add
seannn9 <naeskirlu9@gmail.com>
2024-01-03 17:09:36 +0800

    working map viewer and getting user location

---
+++ lib/services/location.dart
@@ -0,0 +1,26 @@
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  double lon = 0, lat = 0;
+
+  static final Location _instance = Location._internal();
+
+  factory Location() {
+    return _instance;
+  }
+
+  Location._internal();
+
+  Future getMyLocation() async {
+    try {
+      LocationPermission permission = await Geolocator.requestPermission();
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.bestForNavigation
+      );
+      lon = position.longitude;
+      lat = position.latitude;
+    } catch(e) {
+      print(e);
+    }
+  }
+}
